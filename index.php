67a9650b2fe7988dc66dd1fb64b15cc4e9d0131b
jdcarvajalc <jdcc1308@gmail.com>
2024-05-14 16:51:25 -0500

    update index.php and remove 3Index.css

--- index.php
+++ index.php
@@ -1,29 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
+
+<head>
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="shortcut icon" href="public/images/Logo-solo-agost.png"> <!-- Para agregar el ícono favicon -->
-        <title>Inicio de sesión</title>
-        
-       <!-- LIBRERÍAS -->
-        <script src="https://kit.fontawesome.com/00121ead02.js" crossorigin="anonymous"></script>
-        <link href="http://fonts.cdnfonts.com/css/baskerville" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="public/images/Logo-solo-agost.png"> <!-- Para agregar el ícono favicon -->
+    <script src="https://kit.fontawesome.com/00121ead02.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/estilosIndex.css">
+    <title>Inicio de sesión</title>
+</head>
 
-        <link rel="stylesheet" href="public/css/3Index.css">
-
-    </head>
-    <body>
+<body>
+    <div class="contenidoNormal">
         <div class="contenedorImagen">
             <img src="public/images/Logo-general.png" alt="logo de la compañia">
         </div>
-        
+
         <div class="formulario">
-            <img style="border: solid .1px #efffff; background-color: #efffff" src="public/images/Logo-solo-agost.png" alt="logo de la compañia">
+            <img src="public/images/Logo-solo-agost.png" alt="logo de la compañia">
             <h1>Iniciar Sesión </h1>
-            
+
             <form action="app/controllers/controladorInicioSesion.php" method="POST">
                 <!-- USER -->
                 <label for="username">
@@ -40,13 +38,15 @@
                 <input type="password" placeholder="Ingrese contraseña" id="pContra" name="pass" required>
 
                 <input type="submit" name="ingresar" value="INGRESAR">
-
-                <!-- Para Registro -->
-                <div class="referencias">
-                    <a href="registro.php">¡Registrarse!</a>
-                </div>
-                
             </form>
-        </div> 
-    </body>
+        </div>
+    </div>
+    <div class="contenidoEnCelular">
+        <div class="contenedorTarjetaInformativa">
+            <img src="public/images/Logo-general.png" alt="logo de la compañia">
+            <h1>¡Por favor ingrese desde un computador!</h1>
+        </div>
+    </div>
+</body>
+
 </html>
