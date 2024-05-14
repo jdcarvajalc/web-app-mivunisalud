f8141e7f29801cb44e18d94d3a65227544e51ae9
jdcarvajalc <jdcc1308@gmail.com>
2024-05-14 15:32:42 -0500

    update CSS file for graficas page; renamed '5estilosGraficas.css' to 'estilosGraficas.css'

--- app/views/pages/vistaGraficas.php
+++ app/views/pages/vistaGraficas.php
@@ -1,6 +1,5 @@
 <?php require_once '../../config/config.php' ?>
 
-<!-- Para la cabecera con menú principal -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,35 +7,46 @@
   <?php require_once '../includes/head.php' ?>
   <title>Gráficas</title>
   <link rel="shortcut icon" href=<?php echo FROM_PAGES_TO_IMAGES . "/Logo-solo-agost.png" ?> />
-  <link rel="stylesheet" href="<?php echo FROM_PAGES_TO_STYLES . '/5estilosGraficas.css' ?>" />
+  <link rel="stylesheet" href="<?php echo FROM_PAGES_TO_STYLES . '/estilosGraficas.css' ?>" />
 </head>
 
 <body>
 
   <?php require_once '../includes/navbar.php' ?>
 
-  <main style="margin: 20px;">
+  <main>
     <section class="contenedor-mes">
       <form action="vistaGraficas.php" method="POST">
+
         <label for="mes-seleccionado" class="etiqueta-select">Seleccione una fecha: </label>
-        <!-- cambiar a type month -->
+
         <input type="date" name="dia" class="mes-seleccionado" id="mes-seleccionado" value="">
+
         <button id="consultar" type="submit" class="consulta">
           <i class="fa-solid fa-magnifying-glass"></i>
         </button>
+
       </form>
     </section>
-    <section style="margin: 2px 0 0 60px">
-      <div style="margin: 10px 0 0 0; display: flex; justify-content: flex-start; flex-direction: row;">
-        <p style="font-weight: bold; color: #E40C7B;margin: 0; padding-right: 5px">Día: </p>
-        <p style="font-weight: bold; color: #0E2A47; margin:0; padding-right: 20px;" id="presentarDia"></p>
-        <p style="font-weight: bold; color: #E40C7B;margin: 0; padding-right: 5px">Mes: </p>
-        <p style="font-weight: bold; color: #0E2A47; margin:0; padding-right: 20px;" id="presentarMes"></p>
-        <p style="font-weight: bold; color: #E40C7B;margin: 0; padding-right: 5px">Año: </p>
-        <p style="font-weight: bold; color: #0E2A47; margin:0;" id="presentarAnio"></p>
+
+    <section class="seccionInformacionFechas">
+      <div class="contenedorInformacionFechas">
+        <p class="etiquetaInformacionFechas">
+          Día:
+        </p>
+        <p class="informacionFechas" id="presentarDia"></p>
+        <p class="etiquetaInformacionFechas">
+          Mes:
+        </p>
+        <p class="informacionFechas" id="presentarMes"></p>
+        <p class="etiquetaInformacionFechas">
+          Año:
+        </p>
+        <p class="informacionFechas" id="presentarAnio"></p>
       </div>
     </section>
-    <section class="cont" style="margin-top: -40px;">
+
+    <section class="cont">
       <div id="contenido-div1" class="card">
         <div id="cargaLineal" class="mi-grafica"></div>
       </div>
@@ -50,6 +60,4 @@
 
 </html>
 
-<?php
-require_once FROM_PAGES_TO_CONTROLLERS . '/controladorGraficas.php';
-?>
+<?php require_once FROM_PAGES_TO_CONTROLLERS . '/controladorGraficas.php'; ?>
