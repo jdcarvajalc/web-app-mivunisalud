e46cf0b278394fcbfd91ddef4607c01cbd244274
jdcarvajalc <jdcc1308@gmail.com>
2024-05-14 15:01:27 -0500

    comment out database configuration in .gitignore and config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,14 +1,13 @@
 <?php
-    // Configuración de la base de datos
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'dbfarmacia');
-    
-    // Configuración de rutas 
-    define('FROM_PAGES_TO_IMAGES', '../../../public/images');
-    define('FROM_PAGES_TO_STYLES', '../../../public/css');
-    define('FROM_PAGES_TO_JS', '../../../public/js');
-    define('FROM_PAGES_TO_CONTROLLERS', '../../controllers');
-    define('FROM_PAGES_TO_MODELS', '../../models')
-?>
+// Configuración de la base de datos
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'dbfarmacia');
+
+// Configuración de rutas 
+define('FROM_PAGES_TO_IMAGES', '../../../public/images');
+define('FROM_PAGES_TO_STYLES', '../../../public/css');
+define('FROM_PAGES_TO_JS', '../../../public/js');
+define('FROM_PAGES_TO_CONTROLLERS', '../../controllers');
+define('FROM_PAGES_TO_MODELS', '../../models');
